690e122b30c4c1044dce82a9e73a9a5f4ad1db9e
Leo Khachatorians <leokhachatorians@gmail.com>
2021-03-28 15:03:52 -0700

    test to check default starting places

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,11 @@
-#[derive(Debug)]
+//use std::cmp::PartialEq;
+use std::fmt;
+
+
+pub const DEFAULT: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+
+#[derive(Debug, PartialEq)]
 pub enum Peice {
     Pawn,
     Knight,
@@ -9,7 +16,7 @@ pub enum Peice {
     Empty,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Color {
     White,
     Black,
@@ -27,6 +34,12 @@ impl Square {
     }
 }
 
+impl fmt::Display for Square {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({:?}, {:?})", self.peice, self.color)
+    }
+}
+
 pub struct Board {
     pub elements: Vec<Square>
 }
@@ -36,7 +49,6 @@ impl Board {
     pub fn generate_empty_board() -> Board {
         let mut elements: Vec<Square> = Vec::new();
 
-        // Generate Empty Board
         for _ in 0..64 {
         elements.push(
                 Square {
@@ -86,5 +98,60 @@ impl Board {
         }
 
         board
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_default_positions() {
+        let board = Board::new(DEFAULT);
+
+        // Check that White owns these peices
+        for index in 0..16 {
+            assert_eq!(board.elements[index].color, Color::White);
+        }
+
+        // Check White has proper peices
+        assert_eq!(board.elements[0].peice, Peice::Rook);
+        assert_eq!(board.elements[1].peice, Peice::Knight);
+        assert_eq!(board.elements[2].peice, Peice::Bishop);
+        assert_eq!(board.elements[3].peice, Peice::Queen);
+        assert_eq!(board.elements[4].peice, Peice::King);
+        assert_eq!(board.elements[5].peice, Peice::Bishop);
+        assert_eq!(board.elements[6].peice, Peice::Knight);
+        assert_eq!(board.elements[7].peice, Peice::Rook);
+
+        for index in 8..16 {
+            assert_eq!(board.elements[index].peice, Peice::Pawn);
+        }
+
+        // Check that these squares are empty
+        for index in 16..48 {
+            assert_eq!(board.elements[index].peice, Peice::Empty);
+            assert_eq!(board.elements[index].color, Color::Empty);
+        }
+
+        // Check that Black owns these peices
+        for index in 48..64 {
+            assert_eq!(board.elements[index].color, Color::Black);
+        }
+
+        // Check Black as proper peices
+        for index in 48..56 {
+            assert_eq!(board.elements[index].peice, Peice::Pawn);
+        }
+
+        assert_eq!(board.elements[56].peice, Peice::Rook);
+        assert_eq!(board.elements[57].peice, Peice::Knight);
+        assert_eq!(board.elements[58].peice, Peice::Bishop);
+        assert_eq!(board.elements[59].peice, Peice::Queen);
+        assert_eq!(board.elements[60].peice, Peice::King);
+        assert_eq!(board.elements[61].peice, Peice::Bishop);
+        assert_eq!(board.elements[62].peice, Peice::Knight);
+        assert_eq!(board.elements[63].peice, Peice::Rook);
     }
 }
