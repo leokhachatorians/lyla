5d427443f577e872fd2465052a7c5b9818104241
Leo Khachatorians <leokhachatorians@gmail.com>
2021-03-28 15:03:43 -0700

    move default fen string out

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 mod board;
 
-use board::{Board};
-
-const START: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-
+use board::{Board, DEFAULT};
 
 fn main() {
-    let board: Board = Board::new(START);
+    let board: Board = Board::new(DEFAULT);
     for square in board.elements {
         println!("Peice: {:?}\nColor: {:?}\n",
             square.peice, square.color,
